2bd13f7e0af5ae7994d84374e7a4bb7736066f97
Gabriel Félix <gabrielfelix2500@gmail.com>
2021-01-01 18:39:16 -0300

    Organizing Level One and adding fade.

---
+++ Unity Data/Assets/Fade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Fade : MonoBehaviour
+{
+   /* void Update(){
+        Sprite tmpColor = GetComponent<Image>().sprite;
+
+        if(tmpColor.a > 0.0f){
+
+        	Color newColor = new Color(tmpColor.r, tmpColor.g, tmpColor.b, tmpColor.a - Time.deltaTime);
+        	tmpColor.color = newColor;
+        	GetComponent<Image>().sprite = tmpColor;
+        }else{
+        	Destroy(gameObject);
+        }
+    } */
+}

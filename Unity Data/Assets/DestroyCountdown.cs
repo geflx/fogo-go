354d6b45faa06df5bcfb7e596397c5a55070d057
Gabriel Félix <gabrielfelix2500@gmail.com>
2021-01-02 13:16:39 -0300

    Adding "Fade In" effect to Level One.

---
+++ Unity Data/Assets/DestroyCountdown.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCountdown : MonoBehaviour
+{
+	public float destroyTimer;
+
+    void Update(){
+    	destroyTimer -= Time.deltaTime;
+    	if(destroyTimer <= 0.0f)
+    		Destroy(gameObject);        
+    }
+}
